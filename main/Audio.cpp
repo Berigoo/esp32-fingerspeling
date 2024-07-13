2dcf3ff8b9e74853d10fd6516dd683aa6e0ef1fd
Berigoo <muhamadrizqifajri2@gmail.com>
2024-07-13 15:50:55 +0700

    audio

--- main/Audio.cpp
+++ main/Audio.cpp
@@ -1,9 +1,12 @@
 #include "Audio.h"
 
+dac_channel_t Player::dacChannel = DAC_CHAN_0;
+uint32_t Player::sampleRate = 44100;
+
 void Player::setup(const dac_channel_t _dacChannel, const uint32_t _sampleRate){
-  /*dac_output_enable(_dacChannel);*/
-  /*dacChannel = _dacChannel;*/
-  /*sampleRate = _sampleRate;*/
+  dac_output_enable(_dacChannel);
+  Player::dacChannel = _dacChannel;
+  Player::sampleRate = _sampleRate;
 }
 
 void Player::play(int index){
